9251c9106f4aab65f5b412b622c5c20d1d26f77a
Francisco Salinas <remixielive@live.com>
2022-11-11 10:35:36 -0500

    Initial Commit

---
+++ action/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+
+    def search_str(file_path, word):
+        with open(file_path, 'r') as file:
+        # read all content of a file
+            content = file.read()
+            # check if string present in a file
+            if word in content:
+                print('Release Candidate found in package.json! Use only live versions of packages.')
+                sys.exit(1)
+
+    search_str(r'package.json', '-rc.')
+
